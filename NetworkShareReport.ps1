925c099d2872c3a1a3f181e761d40aacd2c937b8
MITS <Billu@advancestuff.com>
2024-03-28 12:35:45 -0400

    Initial commit

---
+++ NetworkShareReport.ps1
@@ -0,0 +1,14 @@
+# Get the network share path
+$sharePath = Read-Host -Prompt "Enter UNC path to share (\\servername\sharename)"
+
+# Get all of the subfolders in the share
+$subfolders = Get-ChildItem -Path $sharePath -Recurse
+
+# Get the permissions for each subfolder
+$permissions = @()
+foreach ($subfolder in $subfolders) {
+    $permissions += Get-Acl -Path $subfolder.FullName
+}
+
+# Export the permissions to a CSV file
+$permissions | Export-Csv -Path "C:\Temp\share_permissions.csv" -NoTypeInformation
