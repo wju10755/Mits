7cb0009cdff7f42af0e06879e35b7a97feea9d03
MITS <Billu@advancestuff.com>
2024-04-22 11:30:45 -0400

    revert

--- Get-LockedOutUser.ps1.ps1
+++ Get-LockedOutUser.ps1.ps1
@@ -17,7 +17,7 @@ function Print-Middle($Message, $Color = "White") {
 $Padding = ("=" * [System.Console]::BufferWidth);
 Write-Host -ForegroundColor "Red" $Padding -NoNewline;
 Print-Middle "MITS - Account Lockout Investigation Script";
-Write-Host -ForegroundColor Cyan "                                                   version 0.1.4";
+Write-Host -ForegroundColor Cyan "                                                   version 0.1.3";
 Write-Host -ForegroundColor "Red" -NoNewline $Padding; 
 Write-Host "  "
 $ErrorActionPreference = "SilentlyContinue"
@@ -71,7 +71,7 @@ foreach ($dc in $domainControllers) {
             LogName = 'Security'
             Id = 4625
         }
-        $events = Get-WinEvent -FilterHashtable $filterHashTable -MaxEvents 1 -ComputerName $dc.HostName
+        $events = Get-WinEvent -FilterHashtable $filterHashTable -MaxEvents 1 -ComputerName $dc.HostName -ErrorAction SilentlyContinue
 
         # Loop through the events and output the required information
         foreach ($event in $events) {
