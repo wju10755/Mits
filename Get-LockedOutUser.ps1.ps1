71bc796c67fb543adc00525cedd7c5559345a245
MITS <Billu@advancestuff.com>
2024-04-22 11:28:44 -0400

    Revert filter

--- Get-LockedOutUser.ps1.ps1
+++ Get-LockedOutUser.ps1.ps1
@@ -25,7 +25,7 @@ $ErrorActionPreference = "SilentlyContinue"
 Import-Module ActiveDirectory
 
 # Get all domain controllers
-$domainControllers = Get-ADDomainController -Filter * | Out-Null
+$domainControllers = Get-ADDomainController -Filter *
 
 # Define the properties to exclude
 $excludedProperties = @('SubjectUserSid', 'SubjectLogonId', 'TargetUserSid', 'Status', 'FailureReason', 'SubStatus', 'TransmittedServices', 'LmPackageName', 'KeyLength', 'ProcessId', 'IpPort')
